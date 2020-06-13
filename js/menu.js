3f3b7d024737e540f649a5135380cc0d16a48033
JulioSolano116 <jcsolano61@misena.edu.co>
2020-06-12 21:28:14 -0500

    Nuevo Inicio

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let botonMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+botonMenu.addEventListener('click' , function(){
+ 'use strict';
+ menu.classList.toggle('mostrar');
+});
